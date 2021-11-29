1426997f90e958a2bae9ccf3bb47d077c388bb99
tomlazersheep <loveratomasg@gmail.com>
2021-11-29 15:56:59 -0300

    levels logic

--- Sokobahn.py
+++ Sokobahn.py
@@ -2,6 +2,8 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import PhotoImage
 
+import time
+
 """
 0-Suelo
 1-Pared
@@ -9,6 +11,12 @@ from tkinter import PhotoImage
 3-Target
 4-Caja fija
 """
+
+# global vars
+
+current_level = 1
+total_steps = 0
+
 
 def crea_matriz_num(level):
     matrix = [[[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -71,10 +79,12 @@ def crea_tablero_img(f, c, m_i, m_n, v_i):
         for j in range(c):
             m_i[i][j] = ttk.Label(image=v_i[m_n[i][j]])
             m_i[i][j].place(x=(32 * i) + 100, y=(32 * j) + 100, width=32, height=32)
+    return m_i
 
 
-def movimiento_jugador(event):
-    print(event.keysym, l_jugador.winfo_x(), l_jugador.winfo_y())
+def movimiento_jugador(event, level_obj):    
+
+    print(event.keysym, level_obj.l_jugador.winfo_x(), level_obj.l_jugador.winfo_y())
     """
     0-Suelo
     1-Pared
@@ -82,112 +92,161 @@ def movimiento_jugador(event):
     3-Target
     4-Caja fija
     """
-    if event.keysym == "Up" and mat_num[iv_col.get()][iv_fil.get() - 1] != 1 and mat_num[iv_col.get()][iv_fil.get() - 1] != 4:
-        pasos.set(pasos.get()+1)
-        if mat_num[iv_col.get()][iv_fil.get() - 1] == 2:
-            if mat_num[iv_col.get()][iv_fil.get() - 2] == 1 or mat_num[iv_col.get()][iv_fil.get() - 2] == 4 or mat_num[iv_col.get()][iv_fil.get() - 2] ==2:
+    if event.keysym == "Up" and level_obj.mat_num[level_obj.iv_col.get()][level_obj.iv_fil.get() - 1] != 1 and level_obj.mat_num[level_obj.iv_col.get()][level_obj.iv_fil.get() - 1] != 4:
+        level_obj.pasos.set(level_obj.pasos.get()+1)
+        if level_obj.mat_num[level_obj.iv_col.get()][level_obj.iv_fil.get() - 1] == 2:
+            if level_obj.mat_num[level_obj.iv_col.get()][level_obj.iv_fil.get() - 2] == 1 or level_obj.mat_num[level_obj.iv_col.get()][level_obj.iv_fil.get() - 2] == 4 or level_obj.mat_num[level_obj.iv_col.get()][level_obj.iv_fil.get() - 2] ==2:
                 return
             else:
-                if mat_num[iv_col.get()][iv_fil.get() - 2] == 0 or mat_num[iv_col.get()][iv_fil.get() - 2] == 3 or mat_num[iv_col.get()][iv_fil.get() - 2] == 1:
-                    if mat_num[iv_col.get()][iv_fil.get() - 2] == 3:
-                        mat_num[iv_col.get()][iv_fil.get() - 2] = 4
-                        mat_img[iv_col.get()][iv_fil.get() - 2].configure(image=vec_img[4])
+                if level_obj.mat_num[level_obj.iv_col.get()][level_obj.iv_fil.get() - 2] == 0 or level_obj.mat_num[level_obj.iv_col.get()][level_obj.iv_fil.get() - 2] == 3 or level_obj.mat_num[level_obj.iv_col.get()][level_obj.iv_fil.get() - 2] == 1:
+                    if level_obj.mat_num[level_obj.iv_col.get()][level_obj.iv_fil.get() - 2] == 3:
+                        level_obj.mat_num[level_obj.iv_col.get()][level_obj.iv_fil.get() - 2] = 4
+                        level_obj.mat_img[level_obj.iv_col.get()][level_obj.iv_fil.get() - 2].configure(image=level_obj.vec_img[4])
                         app.update()
 
-                    elif mat_num[iv_col.get()][iv_fil.get() - 2] == 0:
-                        mat_num[iv_col.get()][iv_fil.get() - 2] = 2
-                        mat_img[iv_col.get()][iv_fil.get() - 2].configure(image=vec_img[2])
+                    elif level_obj.mat_num[level_obj.iv_col.get()][level_obj.iv_fil.get() - 2] == 0:
+                        level_obj.mat_num[level_obj.iv_col.get()][level_obj.iv_fil.get() - 2] = 2
+                        level_obj.mat_img[level_obj.iv_col.get()][level_obj.iv_fil.get() - 2].configure(image=level_obj.vec_img[2])
                         app.update()
 
-                    elif mat_num[iv_col.get()][iv_fil.get() - 2] == 1:
-                        mat_num[iv_col.get()][iv_fil.get() - 1] = 2
-                        mat_img[iv_col.get()][iv_fil.get() - 2].configure(image=vec_img[mat_num[iv_col.get()][iv_fil.get() - 2]])
+                    elif level_obj.mat_num[level_obj.iv_col.get()][level_obj.iv_fil.get() - 2] == 1:
+                        level_obj.mat_num[level_obj.iv_col.get()][level_obj.iv_fil.get() - 1] = 2
+                        level_obj.mat_img[level_obj.iv_col.get()][level_obj.iv_fil.get() - 2].configure(image=level_obj.vec_img[level_obj.mat_num[level_obj.iv_col.get()][level_obj.iv_fil.get() - 2]])
                         app.update()
 
-                    mat_num[iv_col.get()][iv_fil.get() - 1] = 0
-                    mat_img[iv_col.get()][iv_fil.get() - 1].configure(image=vec_img[mat_num[iv_col.get()][iv_fil.get() - 1]])
+                    level_obj.mat_num[level_obj.iv_col.get()][level_obj.iv_fil.get() - 1] = 0
+                    level_obj.mat_img[level_obj.iv_col.get()][level_obj.iv_fil.get() - 1].configure(image=level_obj.vec_img[level_obj.mat_num[level_obj.iv_col.get()][level_obj.iv_fil.get() - 1]])
                     app.update()
                     
-        l_jugador.place(x=l_jugador.winfo_x(), y=l_jugador.winfo_y() - 32)
-        iv_fil.set(iv_fil.get() - 1)
+        level_obj.l_jugador.place(x=level_obj.l_jugador.winfo_x(), y=level_obj.l_jugador.winfo_y() - 32)
+        level_obj.iv_fil.set(level_obj.iv_fil.get() - 1)
 
-    elif event.keysym == "Down" and mat_num[iv_col.get()][iv_fil.get() + 1] != 1 and mat_num[iv_col.get()][iv_fil.get() + 1] != 4:
-        pasos.set(pasos.get()+1)
-        if mat_num[iv_col.get()][iv_fil.get() + 1] == 2:
-            if mat_num[iv_col.get()][iv_fil.get() + 2] == 1 or mat_num[iv_col.get()][iv_fil.get() + 2] == 4 or mat_num[iv_col.get()][iv_fil.get() + 2] ==2:
+    elif event.keysym == "Down" and level_obj.mat_num[level_obj.iv_col.get()][level_obj.iv_fil.get() + 1] != 1 and level_obj.mat_num[level_obj.iv_col.get()][level_obj.iv_fil.get() + 1] != 4:
+        level_obj.pasos.set(level_obj.pasos.get()+1)
+        if level_obj.mat_num[level_obj.iv_col.get()][level_obj.iv_fil.get() + 1] == 2:
+            if level_obj.mat_num[level_obj.iv_col.get()][level_obj.iv_fil.get() + 2] == 1 or level_obj.mat_num[level_obj.iv_col.get()][level_obj.iv_fil.get() + 2] == 4 or level_obj.mat_num[level_obj.iv_col.get()][level_obj.iv_fil.get() + 2] ==2:
                 return
             else:
-                if mat_num[iv_col.get()][iv_fil.get() + 2] == 0 or mat_num[iv_col.get()][iv_fil.get() + 2] == 3:
+                if level_obj.mat_num[level_obj.iv_col.get()][level_obj.iv_fil.get() + 2] == 0 or level_obj.mat_num[level_obj.iv_col.get()][level_obj.iv_fil.get() + 2] == 3:
 
-                    if mat_num[iv_col.get()][iv_fil.get() + 2] == 3:
-                        mat_num[iv_col.get()][iv_fil.get() + 2] = 4
-                        mat_img[iv_col.get()][iv_fil.get() + 2].configure(image=vec_img[4])
+                    if level_obj.mat_num[level_obj.iv_col.get()][level_obj.iv_fil.get() + 2] == 3:
+                        level_obj.mat_num[level_obj.iv_col.get()][level_obj.iv_fil.get() + 2] = 4
+                        level_obj.mat_img[level_obj.iv_col.get()][level_obj.iv_fil.get() + 2].configure(image=level_obj.vec_img[4])
                     else:
-                        mat_num[iv_col.get()][iv_fil.get() + 2] = 2
-                        mat_img[iv_col.get()][iv_fil.get() + 2].configure(image=vec_img[mat_num[iv_col.get()][iv_fil.get() + 2]])
+                        level_obj.mat_num[level_obj.iv_col.get()][level_obj.iv_fil.get() + 2] = 2
+                        level_obj.mat_img[level_obj.iv_col.get()][level_obj.iv_fil.get() + 2].configure(image=level_obj.vec_img[level_obj.mat_num[level_obj.iv_col.get()][level_obj.iv_fil.get() + 2]])
 
-                    mat_num[iv_col.get()][iv_fil.get() + 1] = 0
-                    mat_img[iv_col.get()][iv_fil.get() + 1].configure(image=vec_img[mat_num[iv_col.get()][iv_fil.get() + 1]])
+                    level_obj.mat_num[level_obj.iv_col.get()][level_obj.iv_fil.get() + 1] = 0
+                    level_obj.mat_img[level_obj.iv_col.get()][level_obj.iv_fil.get() + 1].configure(image=level_obj.vec_img[level_obj.mat_num[level_obj.iv_col.get()][level_obj.iv_fil.get() + 1]])
                     app.update()
 
-        l_jugador.place(x=l_jugador.winfo_x(), y=l_jugador.winfo_y() + 32)
-        iv_fil.set(iv_fil.get() + 1)
+        level_obj.l_jugador.place(x=level_obj.l_jugador.winfo_x(), y=level_obj.l_jugador.winfo_y() + 32)
+        level_obj.iv_fil.set(level_obj.iv_fil.get() + 1)
 
-    elif event.keysym == "Right" and mat_num[iv_col.get() + 1][iv_fil.get()] != 1 and mat_num[iv_col.get() + 1][iv_fil.get()] != 4:
-        pasos.set(pasos.get()+1)
-        if mat_num[iv_col.get() + 1][iv_fil.get()] == 2:
-            if mat_num[iv_col.get() +2][iv_fil.get()] == 1 or mat_num[iv_col.get() +2][iv_fil.get()] == 4 or mat_num[iv_col.get() +2][iv_fil.get()] ==2:
+    elif event.keysym == "Right" and level_obj.mat_num[level_obj.iv_col.get() + 1][level_obj.iv_fil.get()] != 1 and level_obj.mat_num[level_obj.iv_col.get() + 1][level_obj.iv_fil.get()] != 4:
+        level_obj.pasos.set(level_obj.pasos.get()+1)
+        if level_obj.mat_num[level_obj.iv_col.get() + 1][level_obj.iv_fil.get()] == 2:
+            if level_obj.mat_num[level_obj.iv_col.get() +2][level_obj.iv_fil.get()] == 1 or level_obj.mat_num[level_obj.iv_col.get() +2][level_obj.iv_fil.get()] == 4 or level_obj.mat_num[level_obj.iv_col.get() +2][level_obj.iv_fil.get()] ==2:
                 return
             else:    
-                if mat_num[iv_col.get() + 2][iv_fil.get()] == 0 or mat_num[iv_col.get() + 2][iv_fil.get()] == 3:
+                if level_obj.mat_num[level_obj.iv_col.get() + 2][level_obj.iv_fil.get()] == 0 or level_obj.mat_num[level_obj.iv_col.get() + 2][level_obj.iv_fil.get()] == 3:
 
-                    if mat_num[iv_col.get() + 2][iv_fil.get()] == 3:
-                        mat_num[iv_col.get() + 2][iv_fil.get()] = 4
-                        mat_img[iv_col.get() + 2][iv_fil.get()].configure(image=vec_img[4])
+                    if level_obj.mat_num[level_obj.iv_col.get() + 2][level_obj.iv_fil.get()] == 3:
+                        level_obj.mat_num[level_obj.iv_col.get() + 2][level_obj.iv_fil.get()] = 4
+                        level_obj.mat_img[level_obj.iv_col.get() + 2][level_obj.iv_fil.get()].configure(image=level_obj.vec_img[4])
                     else:
-                        mat_num[iv_col.get() + 2][iv_fil.get()] = 2
-                        mat_img[iv_col.get() + 2][iv_fil.get()].configure(image=vec_img[mat_num[iv_col.get() + 2][iv_fil.get()]])
+                        level_obj.mat_num[level_obj.iv_col.get() + 2][level_obj.iv_fil.get()] = 2
+                        level_obj.mat_img[level_obj.iv_col.get() + 2][level_obj.iv_fil.get()].configure(image=level_obj.vec_img[level_obj.mat_num[level_obj.iv_col.get() + 2][level_obj.iv_fil.get()]])
 
-                    mat_num[iv_col.get() + 1][iv_fil.get()] = 0
-                    mat_img[iv_col.get() + 1][iv_fil.get()].configure(image=vec_img[mat_num[iv_col.get() + 1][iv_fil.get()]])
+                    level_obj.mat_num[level_obj.iv_col.get() + 1][level_obj.iv_fil.get()] = 0
+                    level_obj.mat_img[level_obj.iv_col.get() + 1][level_obj.iv_fil.get()].configure(image=level_obj.vec_img[level_obj.mat_num[level_obj.iv_col.get() + 1][level_obj.iv_fil.get()]])
                     app.update()
 
-        l_jugador.place(x=l_jugador.winfo_x() + 32, y=l_jugador.winfo_y())
-        iv_col.set(iv_col.get() + 1)
+        level_obj.l_jugador.place(x=level_obj.l_jugador.winfo_x() + 32, y=level_obj.l_jugador.winfo_y())
+        level_obj.iv_col.set(level_obj.iv_col.get() + 1)
 
-    elif event.keysym == "Left" and mat_num[iv_col.get() - 1][iv_fil.get()] != 1 and mat_num[iv_col.get() - 1][iv_fil.get()] != 4:
-        pasos.set(pasos.get()+1)
-        if mat_num[iv_col.get() - 1][iv_fil.get()] == 2:
-            if mat_num[iv_col.get() - 2][iv_fil.get()] == 1 or mat_num[iv_col.get() - 2][iv_fil.get()] == 4 or mat_num[iv_col.get() - 2][iv_fil.get()] ==2:
+    elif event.keysym == "Left" and level_obj.mat_num[level_obj.iv_col.get() - 1][level_obj.iv_fil.get()] != 1 and level_obj.mat_num[level_obj.iv_col.get() - 1][level_obj.iv_fil.get()] != 4:
+        level_obj.pasos.set(level_obj.pasos.get()+1)
+        if level_obj.mat_num[level_obj.iv_col.get() - 1][level_obj.iv_fil.get()] == 2:
+            if level_obj.mat_num[level_obj.iv_col.get() - 2][level_obj.iv_fil.get()] == 1 or level_obj.mat_num[level_obj.iv_col.get() - 2][level_obj.iv_fil.get()] == 4 or level_obj.mat_num[level_obj.iv_col.get() - 2][level_obj.iv_fil.get()] ==2:
                 return
             else:
-                if mat_num[iv_col.get() - 2][iv_fil.get()] == 0 or mat_num[iv_col.get() - 2][iv_fil.get()] == 3:
+                if level_obj.mat_num[level_obj.iv_col.get() - 2][level_obj.iv_fil.get()] == 0 or level_obj.mat_num[level_obj.iv_col.get() - 2][level_obj.iv_fil.get()] == 3:
 
-                    if mat_num[iv_col.get() - 2][iv_fil.get()] == 3:
-                        mat_num[iv_col.get() - 2][iv_fil.get()] = 4
-                        mat_img[iv_col.get() - 2][iv_fil.get()].configure(image=vec_img[4])
+                    if level_obj.mat_num[level_obj.iv_col.get() - 2][level_obj.iv_fil.get()] == 3:
+                        level_obj.mat_num[level_obj.iv_col.get() - 2][level_obj.iv_fil.get()] = 4
+                        level_obj.mat_img[level_obj.iv_col.get() - 2][level_obj.iv_fil.get()].configure(image=level_obj.vec_img[4])
                     else:
-                        mat_num[iv_col.get() - 2][iv_fil.get()] = 2
-                        mat_img[iv_col.get() - 2][iv_fil.get()].configure(image=vec_img[mat_num[iv_col.get() - 2][iv_fil.get()]])
+                        level_obj.mat_num[level_obj.iv_col.get() - 2][level_obj.iv_fil.get()] = 2
+                        level_obj.mat_img[level_obj.iv_col.get() - 2][level_obj.iv_fil.get()].configure(image=level_obj.vec_img[level_obj.mat_num[level_obj.iv_col.get() - 2][level_obj.iv_fil.get()]])
 
-                    mat_num[iv_col.get() - 1][iv_fil.get()] = 0
-                    mat_img[iv_col.get() - 1][iv_fil.get()].configure(image=vec_img[mat_num[iv_col.get() - 1][iv_fil.get()]])
+                    level_obj.mat_num[level_obj.iv_col.get() - 1][level_obj.iv_fil.get()] = 0
+                    level_obj.mat_img[level_obj.iv_col.get() - 1][level_obj.iv_fil.get()].configure(image=level_obj.vec_img[level_obj.mat_num[level_obj.iv_col.get() - 1][level_obj.iv_fil.get()]])
                     app.update()
 
-        l_jugador.place(x=l_jugador.winfo_x() - 32, y=l_jugador.winfo_y())
-        iv_col.set(iv_col.get() - 1)
+        level_obj.l_jugador.place(x=level_obj.l_jugador.winfo_x() - 32, y=level_obj.l_jugador.winfo_y())
+        level_obj.iv_col.set(level_obj.iv_col.get() - 1)
 
-    steps_label_var.set(f"Pasos: {pasos.get()}")
     
-    #check for winning: no value 2 or 3
+    #check for winning: no values 2 or 3
     win_flag = True
-    for mat_list in mat_num:
+    for mat_list in level_obj.mat_num:
       for mat_number in mat_list:
         if mat_number == 2 or mat_number == 3:
           win_flag = False
           break
     if win_flag:
-      print("win!") #draw next level?
+      global current_level
+      current_level += 1
+      global total_steps
+      total_steps += level_obj.pasos.get()
+      global current_level_obj
+      current_level_obj.destroy_level()
+
+      if (current_level <= 3):
+        current_level_obj = Level(current_level)
+        current_level_obj.draw_level()
+      else: 
+        Label(app, text=f"Game finished.").pack()
+        Label(app, text=f"Steps total: {total_steps}").pack()
+        global reset_btn
+        reset_btn.destroy()
+        Button(app, text="Exit", command=lambda: app.destroy()).pack()
+    else: 
+      level_obj.steps_label_var.set(f"Pasos: {level_obj.pasos.get()}")
+
+class Level:
+  def __init__(self, level_number):
+    self.mat_img = crea_matriz_img(10, 10)
+    self.mat_num = crea_matriz_num(level_number)
+    self.vec_img = crea_vector_img()
+
+    self.iv_fil = IntVar()
+    self.iv_col = IntVar()
+    self.iv_col.set(4)
+    self.iv_fil.set(4)
+
+    self.pasos=IntVar()
+    self.pasos.set(0)
+
+  def draw_level(self):
+    self.board = crea_tablero_img(10, 10, self.mat_img, self.mat_num, self.vec_img)
+
+    self.l_jugador = ttk.Label(image=self.vec_img[5])
+    self.l_jugador.place(x=(32 * self.iv_col.get()) + 100, y=(32 * self.iv_fil.get()) + 100, width=32, height=32)
+
+    self.steps_label_var = StringVar()
+    self.steps_label_var.set(f"Pasos: {self.pasos.get()}")
+    self.steps_label = ttk.Label(textvariable=self.steps_label_var, font=('MathJax_SansSerif-Bold', 14, "bold"))
+    self.steps_label.pack()
+
+
+  def destroy_level(self):
+    for line in self.board:
+      for elem in line:
+        elem.destroy()
+    self.l_jugador.destroy()
+    self.steps_label.destroy()
 
 # PROGRAMA
 
@@ -196,30 +255,25 @@ app = Tk()
 app.geometry("500x500")
 app.title("Sokobahn")
 
-current_level = 1
-total_steps = 0
+current_level_obj = Level(1)
 
-iv_fil = IntVar()
-iv_col = IntVar()
-iv_col.set(4)
-iv_fil.set(4)
+current_level_obj.draw_level()
+app.bind("<Key>", lambda event: movimiento_jugador(event, current_level_obj))
 
-mat_img = crea_matriz_img(10, 10)
-mat_num = crea_matriz_num(current_level)
-vec_img = crea_vector_img()
 
-pasos=IntVar()
-pasos.set(0)
+def destroy_level():
+  current_level_obj.destroy_level()
 
-crea_tablero_img(10, 10, mat_img, mat_num, vec_img)
+def draw_level():
+  current_level_obj.draw_level()
 
-l_jugador = ttk.Label(image=vec_img[5])
-l_jugador.place(x=(32 * iv_col.get()) + 100, y=(32 * iv_fil.get()) + 100, width=32, height=32)
+def reset_level():
+  global current_level_obj
+  current_level_obj.destroy_level()
+  current_level_obj = Level(current_level)
+  current_level_obj.draw_level()
 
-steps_label_var = StringVar()
-steps_label_var.set(f"Pasos: {pasos.get()}")
-steps_label = ttk.Label(textvariable=steps_label_var, font=('MathJax_SansSerif-Bold', 14, "bold")).pack()
-
-app.bind("<Key>", movimiento_jugador)
+reset_btn = Button(app, text="reset level", command=lambda: reset_level())
+reset_btn.place(x=350, y=10)
 
 app.mainloop()
